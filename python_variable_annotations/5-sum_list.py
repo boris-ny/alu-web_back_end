d864b33995c8836e1fe305d9a75da22f424f74dc
boris-ny <b.nyilindek@alustudent.com>
2024-05-06 14:39:26 +0200

    chore: Update type annotations for sum_list and sum_mixed_list functions

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function sum_list which takes a list input_list of floats as argument and returns their sum as a float.
+"""Write a type-annotated function sum_list.
 """
 from typing import List
 
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Write a type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float.
+Write a type-annotated function sum_mixed_list 
 """
 from typing import Union, List
 
