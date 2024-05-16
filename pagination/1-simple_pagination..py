2f3d28437324d1b7b8516547b70e6fe79f96735d
boris-ny <b.nyilindek@alustudent.com>
2024-05-16 23:40:07 +0200

    pagination

---
+++ pagination/1-simple_pagination..py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """Return a simple helper function caontaining start and end index"""
+    if (page == 1):
+        return (0, page_size)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """class to paginate csv file"""
+    DATA_FILE = "./Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """load the data from the csv file"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as csvfile:
+                reader = csv.reader(csvfile)
+                dataset = [row for row in reader]
+                self.__dataset = dataset[1:]
+                return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get the page data"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        page_info = index_range(page=page, page_size=page_size)
+        data = self.dataset()
+        if page_info[1] > len(data):
+            return []
+        return data[page_info[0]:page_info[1]]
