6901f6ae293ec134bbcab9ba04cc554365e8e20f
boris-ny <b.nyilindek@alustudent.com>
2024-05-22 18:54:44 +0200

    refactor: Update filter_datum function signature to include message parameter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ import mysql.connector
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
-def filter_datum(fields: List[str], redaction: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """"returns the log message obfuscated"""
     for field in fields:
         message = re.sub(field + "=.*?" + separator, field +
