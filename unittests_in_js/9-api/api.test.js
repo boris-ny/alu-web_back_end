f38a34509a15b0e1b0257b95d5ee4871b844105b
boris-ny <b.nyilindek@alustudent.com>
2024-07-19 21:26:27 +0200

    fix cart route

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,12 +1,11 @@
 const request = require('request');
 const chai = require('chai');
 const { expect } = require('chai');
-const { describe } = require('mocha');
 
-describe('server', () => {
-  it('Index Page', (done) => {
+describe('/ api', () => {
+  it('status 200', (done) => {
     request('http://localhost:7865', (error, response, body) => {
-      if (error) throw new Error(error);
+      if (error) throw error;
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
@@ -15,20 +14,19 @@ describe('server', () => {
 });
 
 describe('GET /cart/:id', () => {
-  it('404 - Id is not a number', (done) => {
-    request('http://localhost:7865/cart/anything', (error, response, body) => {
-      if (error) throw new Error(error);
+  it('status 404', (done) => {
+    request('http://localhost:7865/cart/asd', (error, response) => {
+      if (error) throw error;
       expect(response.statusCode).to.equal(404);
-      expect(body).to.equal('Id is not a number');
       done();
     });
   });
 
-  it('200 - Payment methods for cart 1', (done) => {
-    request('http://localhost:7865/cart/1', (error, response, body) => {
-      if (error) throw new Error(error);
+  it('status 200', (done) => {
+    request('http://localhost:7865/cart/400', (error, response, body) => {
+      if (error) throw error;
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 1');
+      expect(body).to.equal('Payment methods for cart 400');
       done();
     });
   });
